de2082cc315c6ca9c5bdb80b501b3f2694222b7e
fabio italiano <faitalia@hotmail.com>
2022-07-14 15:16:11 -0300

    js

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,10 +3,13 @@ import clientPromise from '../lib/mongodb'
 
 export default function Index({ isConnected }) {
   return (
-    <p>
-      To test the CORS route, open the console in a new tab on a different
-      domain and make a POST / GET / OPTIONS request to <b>/api/cors</b>. Using
-      a different method from those mentioned will be blocked by CORS
-    </p>
+    <><small>isConnected: {isConnected}</small>
+      <p>
+        To test the CORS route, open the console in a new tab on a different
+        domain and make a POST / GET / OPTIONS request to <b>/api/cors</b>. Using
+        a different method from those mentioned will be blocked by CORS
+      </p>
+    </>
+
   )
 }
